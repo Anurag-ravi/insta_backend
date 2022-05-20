97ab5b0dc2e198e2f5380a98eaf9632ef4dd1e60
Anurag Ravi <kumarayush1014@gmail.com>
2022-05-20 18:36:36 +0530

    n

--- insta_backend/settings.py
+++ insta_backend/settings.py
@@ -135,7 +135,7 @@ APPEND_SLASH=False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [
     BASE_DIR / "static/",
 ]
